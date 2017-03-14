5aa407e40f0bc68cae8dfa55a36ad7158734fdb2
rajasricity <sanarajasekhar@gmail.com>
2017-03-14 16:12:47 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+var server = "https://panaromic.com/application/";
+$(document).ready(function(){
+$("#login").on('submit', function(e){
+   e.preventDefault();
+   var fdata = $("#login").serialize();
+   $.ajax({
+      url:server+"checkLogin.php",
+      data:fdata,
+      type:"post",
+      beforeSend: function(){
+        $("#loader").show();
+      },
+      success: function(str){
+        $("#loader").hide();
+        if(str == 'Wrong'){
+          $("#wmsg").show();
+          $("#mnumber").val('');
+          $("#pword").val('');
+        }else{
+        $("#wmsg").hide();
+        alert(str.Role);
+        //localStorage.setItem("Userno",str);
+        //location.href="Dashboard.html";
+        }
+      }
+   });
+});
+});
